77bce706277d1f164d7a955edaada4c45b99d003
Andrey Perestoronin <per4ik256@gmail.com>
2015-10-19 09:31:32 +0300

    var names changed

--- string-function-calculation-v2.py
+++ string-function-calculation-v2.py
@@ -3,41 +3,41 @@ import doctest
 
 def suffix_array(text):
     indexes_suff_arr, suff_arr, lbound = [0] * len(text), [0] * len(text), [0] * len(text)
-    for i in xrange(len(text)):
-        suff_arr[i] = ord(text[i])
+    for ind in xrange(len(text)):
+        suff_arr[ind] = ord(text[ind])
 
     size = 1
     while size <= len(text):
-        for i in xrange(len(lbound)):
-            if i + size < len(text):
-                lbound[i] = ((suff_arr[i], suff_arr[i + size]), i)
+        for ind in xrange(len(lbound)):
+            if ind + size < len(text):
+                lbound[ind] = ((suff_arr[ind], suff_arr[ind + size]), ind)
             else:
-                lbound[i] = ((suff_arr[i], -1), i)
+                lbound[ind] = ((suff_arr[ind], -1), ind)
         lbound.sort()
         suff_arr[lbound[0][1]] = 0
-        for i in xrange(1, len(lbound)):
-            cls, index = lbound[i]
-            if cls == lbound[i - 1][0]:
-                suff_arr[index] = suff_arr[lbound[i - 1][1]]
+        for ind in xrange(1, len(lbound)):
+            cls, index = lbound[ind]
+            if cls == lbound[ind - 1][0]:
+                suff_arr[index] = suff_arr[lbound[ind - 1][1]]
             else:
-                suff_arr[index] = suff_arr[lbound[i - 1][1]] + 1
+                suff_arr[index] = suff_arr[lbound[ind - 1][1]] + 1
         size *= 2
 
-    for i, p in enumerate(suff_arr):
-        indexes_suff_arr[p] = i
+    for ind, p in enumerate(suff_arr):
+        indexes_suff_arr[p] = ind
     return indexes_suff_arr, suff_arr
 
 
 def lcp(text, _suff_arr_, rank):
     lcp_val = [0] * len(_suff_arr_)
     height = 0
-    for i in xrange(len(text)):
-        if rank[i] == 0:
+    for ind in xrange(len(text)):
+        if rank[ind] == 0:
             continue
-        j = _suff_arr_[rank[i] - 1]
-        while text[i + height] == text[j + height]:
+        j = _suff_arr_[rank[ind] - 1]
+        while text[ind + height] == text[j + height]:
             height += 1
-        lcp_val[rank[i]] = height
+        lcp_val[rank[ind]] = height
         if height > 0:
             height -= 1
     return lcp_val
